547fa0b7796d94a97fe5942a4cf88e039a701b4a
Paul Gurnig <pgurnig@gmail.com>
2024-01-20 14:51:30 -0600

    Update to git-recurse-status.sh

--- bash/git-recurse-status.sh
+++ bash/git-recurse-status.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 clear
+FILENAME='/tmp/.git-recurse-status.sh.tmp'
+# OUTPUT=''
 baseDir="."
 readarray -d '' arr < <(find "${baseDir}" -mindepth 1 -maxdepth 1 -type d -print0 | sort -z)
 
-for x in ${arr[@]}; do
-  cleandir=`echo $x | cut -c 3-`
-  echo "================================================="
-  echo ${cleandir}
+echo "" > $FILENAME
+
+for x in "${arr[@]}"; do
+  cleandir=`echo $x | cut -c 3-` >> $FILENAME
+  echo "=================================================" >> $FILENAME
+  OUTPUT="${OUTPUT}====================="
+  echo ${cleandir} >> $FILENAME 
   cd ${cleandir}
-  git status $1
+  git status $1 >> $FILENAME 
   cd ..
 done
-exit
 
+# Temporary means of doing this; need to wildcard on 'On branch' string
+sed -i 's/On branch main//g' $FILENAME
+sed -i 's/On branch master//g' $FILENAME
+sed -i '/^$/d' $FILENAME
+cat $FILENAME
