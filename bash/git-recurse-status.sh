82531325a45e81ffbd4ebd8af839b22c087c7849
Paul Gurnig <pgurnig@gmail.com>
2024-01-21 12:27:29 -0600

    Incremental changes to git-recurse-status.sh

--- bash/git-recurse-status.sh
+++ bash/git-recurse-status.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+# Clear the screen
 clear
-FILENAME='/tmp/.git-recurse-status.sh.tmp'
-# OUTPUT=''
+# Write to the /tmp directory. We are assuming Linux.
+FILENAME='/tmp/git-recurse-status.sh.tmp'
+OUTPUT=''
 baseDir="."
 readarray -d '' arr < <(find "${baseDir}" -mindepth 1 -maxdepth 1 -type d -print0 | sort -z)
 
-echo "" > $FILENAME
+echo "$(date)" > $FILENAME
 
 for x in "${arr[@]}"; do
-  cleandir=`echo $x | cut -c 3-` >> $FILENAME
+  cleandir=$(echo $x | cut -c 3-) >> $FILENAME
+  # echo ${cleardir}
   echo "=================================================" >> $FILENAME
-  OUTPUT="${OUTPUT}====================="
+  OUTPUT="${OUTPUT}${cleandir}|"
   echo ${cleandir} >> $FILENAME 
   cd ${cleandir}
-  git status $1 >> $FILENAME 
+  # redirect git status with any included parms to the file; same for stderr
+  git status $1 >>$FILENAME 2>&1 
   cd ..
 done
 
@@ -23,3 +27,4 @@ sed -i 's/On branch main//g' $FILENAME
 sed -i 's/On branch master//g' $FILENAME
 sed -i '/^$/d' $FILENAME
 cat $FILENAME
+echo $OUTPUT
